9e2215939c51c256da0abc7604480c26ade36e59
James Butcher <jamesbutcher167@gmail.com>
2021-07-30 15:40:09 +0100

    Fix syntax error

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { promises: fs } = require('fs');
+const fs = require('fs');
 const yaml = require('js-yaml');
 
 try {
   const file = core.getInput('file');
   const key = core.getInput('key');
 
-  let content = await fs.readFile(file, 'utf8');
+  let content = fs.readFile(file, 'utf8');
   let yamlData = yaml.load(content);
 
-  core.setOutput('data', yamlData[key])
+  core.setOutput('data', yamlData[key]);
+} catch (e) {
+  console.log(e);
 }
