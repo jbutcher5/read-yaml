36d653c65faa8c9aa52b78e1ec953552d2d0ea0e
James Butcher <jamesbutcher167@gmail.com>
2022-10-24 20:38:50 +0100

    Replace setOutput with saveState

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,24 @@ import { promises as fs } from 'fs'
 import * as yaml from 'js-yaml'
 
 const run = async () => {
-    try {
-        const file = core.getInput('file')
-        const keys: string[] = JSON.parse(core.getInput('key-path'))
+  try {
+    const file = core.getInput('file')
+    const keys: string[] = JSON.parse(core.getInput('key-path'))
 
-        const content = await fs.readFile(file, 'utf8')
+    const content = await fs.readFile(file, 'utf8')
 
-        let yamlData = yaml.load(content)
+    let yamlData = yaml.load(content)
 
-        if (yamlData == null || yamlData == undefined) {
-            core.setFailed('Error in reading the yaml file')
-            return
-        }
-
-        let output = keys.reduce((dict, key) => dict[key], yamlData)
-        core.setOutput('data', output)
-    } catch (error) {
-        core.setFailed((error as Error).message)
+    if (yamlData == null || yamlData == undefined) {
+      core.setFailed('Error in reading the yaml file')
+      return
     }
+
+    let output = keys.reduce((dict, key) => dict[key], yamlData)
+    core.saveState('data', output)
+  } catch (error) {
+    core.setFailed((error as Error).message)
+  }
 }
 
 run()
